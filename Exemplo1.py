785a85f1ef0c32f30dd1b873aa05d2f5c64b83f1
maynemed <maynemedeiros@hotmail.com.br>
2025-08-18 19:56:00 -0300

    Add files via upload

---
+++ Exemplo1.py
@@ -0,0 +1,2 @@
+for i in range (3):
+    idade = int(input("Digita a idade do cliente: "))
---
+++ Exemplo3.py
@@ -0,0 +1,4 @@
+clientes = int(input("Informe a quantidade de Clientes:"))
+
+for i in range (clientes):
+    nome = input("Digite o nome do cliente:")
